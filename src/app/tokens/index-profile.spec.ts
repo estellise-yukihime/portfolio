fc374a81e622b73a70e07f4740fffcf9f15b505c
estellise-yukihime <shiranoyukihime@gmail.com>
2026-07-08 20:43:12 +0800

    feat: add index profile token and spec file

---
+++ src/app/tokens/index-profile.spec.ts
@@ -0,0 +1,7 @@
+import { IndexProfile } from './index-profile'
+
+describe('IndexProfile', () => {
+  it('should create an instance', () => {
+    expect(new IndexProfile()).toBeTruthy()
+  })
+})
---
+++ src/app/tokens/index-profile.ts
@@ -0,0 +1,3 @@
+import { InjectionToken } from '@angular/core'
+
+export const INDEX_PROFILE_TOKEN = new InjectionToken<string>('profile.id');
