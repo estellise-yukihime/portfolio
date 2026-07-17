1d93141ee13e37434db82c0474a195ed73acf993
estellise-yukihime <shiranoyukihime@gmail.com>
2026-07-17 21:57:00 +0800

    feat: update profile-api service with new endpoints and models

--- src/app/services/api/profile-api/profile-api.ts
+++ src/app/services/api/profile-api/profile-api.ts
@@ -1,37 +1,31 @@
 import { inject, Service } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
-import { Profile } from '../../../models/profile'
 import { environment } from '../../../../environments/environment'
 import { ProfileHero } from '../../../models/profile-hero'
-import { ProfileLink } from '../../../models/profile-link'
-import { ProfileInfo } from '../../../models/profile-info'
+import { ProfileNavi } from '../../../models/profile-navi'
+import { Paginated } from '../../../models/paginated'
+import { ProfileCard } from '../../../models/profile-card'
 
 @Service()
 export class ProfileApi {
   private readonly _httpClient = inject(HttpClient)
 
-  getProfile(id: string): Observable<Profile> {
-    return this._httpClient.get<Profile>(
-      `${environment.api.url}/api/${environment.api.version}/profiles/${id}`
+  getProfilesCard(): Observable<Paginated<ProfileCard>> {
+    return this._httpClient.get<Paginated<ProfileCard>>(
+      `${environment.api.url}/api/v1/profiles/card`
+    )
+  }
+
+  getProfileNavi(id: string): Observable<ProfileNavi> {
+    return this._httpClient.get<ProfileNavi>(
+      `${environment.api.url}/api/${environment.api.version}/profiles/${id}/navi`
     )
   }
 
   getProfileHero(id: string): Observable<ProfileHero> {
     return this._httpClient.get<ProfileHero>(
       `${environment.api.url}/api/${environment.api.version}/profiles/${id}/hero`
-    )
-  }
-
-  getProfileLink(id: string): Observable<ProfileLink[]> {
-    return this._httpClient.get<ProfileLink[]>(
-      `${environment.api.url}/api/${environment.api.version}/profiles/${id}/link`
-    )
-  }
-
-  getProfileInfo(id: string): Observable<ProfileInfo> {
-    return this._httpClient.get<ProfileInfo>(
-      `${environment.api.url}/api/${environment.api.version}/profiles/${id}/info`
     )
   }
 }
