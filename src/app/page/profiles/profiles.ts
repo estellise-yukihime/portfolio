aa4ef957cdd21bf9c6cedd6dd1ce1f4cea8b4376
estellise-yukihime <shiranoyukihime@gmail.com>
2026-07-17 21:57:40 +0800

    feat: update profiles page to use new `ProfileCard` and `Paginated` models

--- src/app/page/profiles/profiles.ts
+++ src/app/page/profiles/profiles.ts
@@ -1,38 +1,44 @@
-import { Component } from '@angular/core'
-import { Footer } from '../../shared/footer/footer'
+import { Component, input } from '@angular/core'
 import { NavEmpty } from '../../shared/nav-empty/nav-empty'
 import { Pagination } from '../../shared/pagination/pagination'
 import { Reveal } from '../../shared/reveal'
 import { RouterLink } from '@angular/router'
 import { FooterEmpty } from '../../shared/footer-empty/footer-empty'
-
-type TempItem = {
-  id: number
-  name: string
-  nameInitial: string
-  title: string
-  summary: string
-  stack: string[]
-  location: string
-}
+import { ProfileCard } from '../../models/profile-card'
+import { Paginated } from '../../models/paginated'
 
 @Component({
   selector: 'app-profiles',
-  imports: [Footer, NavEmpty, Pagination, Reveal, RouterLink, FooterEmpty],
+  imports: [NavEmpty, Pagination, Reveal, RouterLink, FooterEmpty],
   templateUrl: './profiles.html',
   styleUrl: './profiles.css'
 })
 export class Profiles {
-  items: TempItem[] = [
-    {
-      id: 0,
-      name: 'Sample',
-      nameInitial: 'SS',
-      title: '.NET Developer',
-      summary:
-        'A software developer of 7+ years building reliable, maintainable, and scalable systems.',
-      stack: ['C#', '.NET', 'ASP.NET'],
-      location: 'Cebu'
+  readonly paginated = input.required<Paginated<ProfileCard>>()
+
+  get profilesCard(): ProfileCard[] {
+    return this.paginated().items
+  }
+
+  name(card: ProfileCard): string {
+    return `${card.first_name ?? ''} ${card.last_name ?? ''}`.trim()
+  }
+
+  initial(card: ProfileCard): string {
+    const initialF = card.first_name?.slice(0, 1)
+    const initialL = card.last_name?.slice(0, 1)
+
+    return `${initialF}${initialL}`.toUpperCase()
+  }
+
+  skills(card: ProfileCard): string[] {
+    const rank = card.skills?.sort((a, b) => b.proficiency - a.proficiency) ?? []
+    const tree = rank.slice(0, 3).map((skill) => skill.name as string)
+
+    if (card.skills?.length && card.skills?.length > 3) {
+      return [...tree, '...']
     }
-  ]
+
+    return tree
+  }
 }
