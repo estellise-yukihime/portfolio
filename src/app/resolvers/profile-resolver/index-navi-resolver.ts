91e16aecadf904ffdd57ed1e84bb6737648710a1
estellise-yukihime <shiranoyukihime@gmail.com>
2026-07-17 21:57:12 +0800

    feat: refactor profile resolvers to use updated models and API methods

--- src/app/resolvers/profile-resolver/index-navi-resolver.ts
+++ src/app/resolvers/profile-resolver/index-navi-resolver.ts
@@ -1,10 +1,10 @@
-import { ResolveFn } from '@angular/router'
-import { ProfileInfo } from '../../models/profile-info'
-import { inject } from '@angular/core'
+import { ResolveFn }   from '@angular/router'
+import { ProfileNavi } from '../../models/profile-navi'
+import { inject }      from '@angular/core'
 import { ProfileApi } from '../../services/api/profile-api/profile-api'
 
-export const indexNaviResolver: ResolveFn<ProfileInfo> = (route, state) => {
+export const indexNaviResolver: ResolveFn<ProfileNavi> = (route, state) => {
   const profileApi = inject(ProfileApi)
 
-  return profileApi.getProfileInfo(route.data['profileId'])
+  return profileApi.getProfileNavi(route.data['profileId'])
 }
--- src/app/resolvers/profile-resolver/profile-navi-resolver.ts
+++ src/app/resolvers/profile-resolver/profile-navi-resolver.ts
@@ -1,10 +1,10 @@
 import { ResolveFn } from '@angular/router'
 import { inject } from '@angular/core'
-import { ProfileApi } from '../../services/api/profile-api/profile-api'
-import { ProfileInfo } from '../../models/profile-info'
+import { ProfileApi }  from '../../services/api/profile-api/profile-api'
+import { ProfileNavi } from '../../models/profile-navi'
 
-export const profileNaviResolver: ResolveFn<ProfileInfo> = (route, state) => {
+export const profileNaviResolver: ResolveFn<ProfileNavi> = (route, state) => {
   const profileApi = inject(ProfileApi)
 
-  return profileApi.getProfileInfo(route.params['profileId'])
+  return profileApi.getProfileNavi(route.params['profileId'])
 }
